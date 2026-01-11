e296a92116c668cb44cf98854f3d78e761fa5b15
Meet Savani <meetsavani75@gmail.com>
2026-01-11 13:55:03 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,104 @@
+import express from 'express';
+import mongoose, { Schema, model } from 'mongoose';
+import cors from 'cors';
+import { nanoid } from 'nanoid';
+import isURL from 'validator/lib/isURL.js'; 
+import dotenv from 'dotenv';
+import QRCode from 'qrcode';
+dotenv.config();
+
+const app = express();
+app.use(cors({
+    origin: '*'
+}));
+app.use(express.json());
+const PORT = 5000;
+
+// Connect to MongoDB 
+mongoose.connect(process.env.DATABASE_URL, {
+    serverSelectionTimeoutMS: 30000, // Keep trying to send operations for 30 seconds
+    socketTimeoutMS: 45000 // Close sockets after 45 seconds of inactivity
+})
+    .then(() => console.log("Connected to MongoDB Atlas"))
+    .catch((err) => {
+        console.error("MongoDB connection error:", err.message);
+    });
+
+const urlSchema = new Schema({
+    originalUrl: {
+        type: String,
+        required: true,
+    },
+    shortCode: {
+        type: String,
+        required: true, 
+        unique: true,
+        sparse: true, 
+    },
+    clicks: {
+        type: Number,
+        default: 0,
+    }
+});
+
+const Url = model('Url', urlSchema);
+
+app.post('/api/short', async (req, res) => {
+    try {
+        console.log("📩 Received POST /api/short:", req.body);
+
+        const { originalUrl } = req.body;
+        if(!originalUrl) {
+            return res.status(400).json({ message: "Original URL is required" });
+        }
+
+        // if (!process.env.BASE_URL) {
+        //     return res.status(500).json({
+        //         message: "BASE_URL is not configured on the server"
+        //     });
+        // }
+
+        if (!isURL(originalUrl, { require_protocol: true })) {
+            return res.status(400).json({
+                message: "URL must include http:// or https://"
+            });
+        }        
+
+        const shortCode = nanoid(7);
+
+        const url = new Url({ originalUrl, shortCode });
+        await url.save();
+
+        const myUrl = `http://localhost:5000/${shortCode}`;
+        const qrCodeImg = await QRCode.toDataURL(myUrl);
+
+        res.status(200).json({ message: "URL shortened successfully",  shortUrl: myUrl, qrCodeImg });
+    } catch (error) {
+        console.error("🔥Error in /api/short:", error);
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+});
+
+app.get('/:shortCode', async (req, res) => {
+    try {
+        const { shortCode } = req.params;
+        const url = await Url.findOne({ shortCode });
+        console.log("URL found: ", url);
+
+        if(url) {
+            url.clicks++;
+            await url.save();
+
+            return res.redirect(url.originalUrl);
+        } else {
+            return res.status(404).json({ message: "URL not found" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react"
+import axios from "axios";
+
+function App() {
+
+  const [originalUrl, setOriginalUrl] = useState('');
+  const [shortUrl, setShortUrl] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); 
+
+    try {
+      const res = await axios.post('http://localhost:5000/api/short', { originalUrl });
+      console.log("Response: ", res);
+      setShortUrl(res.data);
+      console.log(res.data);
+    } catch(err) {
+      console.error("Error in App.jsx: ", err);
+    }
+  }
+
+  return (
+    <div className='min-h-screen flex items-center justify-center bg-gray-100 p-4'>
+      <div className="bg-white rounded-lg shadow-lg p-4 sm:p-8 max-w-lg w-full">
+        <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-center mb-6 text-blue-600">URL Shortener</h1>
+        <form onSubmit={handleSubmit} className="flex flex-col space-y-2 sm:space-y-4">
+          <input 
+            type="text" name="originalUrl" 
+            placeholder="Enter your URL here"
+            value={originalUrl}
+            required
+            onChange={(e) => setOriginalUrl(e.target.value)}
+            className="border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 text-sm sm:text-base" />
+
+          <button 
+            type="submit" 
+            className="bg-blue-600 text-white md:text-xl rounded-md p-2 font-semibold hover:bg-blue-700 transition">
+              Shorten
+          </button>
+        </form>
+
+          { shortUrl && (
+            <div className="mt-6 text-center">
+              <p className="text-gray-700 sm:text-lg md:text-xl font-medium">Shortened URL:</p>
+              <a 
+                href={shortUrl.shortUrl} 
+                target="_blank" 
+                rel="noopener noreferrer" 
+                className="text-blue-600 font-semibold text-sm sm:text-base md:text-lg hover:underline break-all">
+                  {shortUrl.shortUrl}
+              </a>
+
+              {shortUrl && (
+                <div className="mt-2 sm:mt-4">
+                  <p className="text-gray-700 text-lg md:text-xl font-medium sm:mb-2">QR Code:</p>
+                  <img src={shortUrl.qrCodeImg} alt="QR Code" className="mx-auto" />
+                </div>
+              )}
+            </div>
+          )}
+      </div>
+    </div>
+  )
+}
+
+export default App
